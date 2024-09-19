a7b5f4ebda6cdaf4d4a141a9e3725645b692fe49
kenza17-urc <84207524+kenza17-urc@users.noreply.github.com>
2024-09-19 09:34:02 +0100

    Update CategoryNav.js

--- src/components/CategoryNav.js
+++ src/components/CategoryNav.js
@@ -10,7 +10,7 @@ const CategoryNav = () => {
     <aside className='hidden xl:flex'>
       <div className='bg-primary flex flex-col w-[286px] h-[500px] rounded-[8px] overflow-hidden'>
         <div className='bg-accent py-4 text-primary uppercase font-semibold flex items-center justify-center'>
-          Categoris
+          Categories
         </div>
         <div className='flex flex-col gap-y-6 p-6'>
           {data?.map((category) => {
